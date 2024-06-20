d5c0186007b90a25b325e27b361f7225d1973035
kturaevv <kturaevv@gmail.com>
2024-06-20 10:23:17 +0500

    add: instance creation

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,7 @@ void run() {
 
 private:
     GLFWwindow* _window;
+    VkInstance _instance;
 
 private:
     void initWindow() {
@@ -30,7 +31,7 @@ private:
     }
     
     void initVulkan() {
-
+        createInstance();
     }
 
     void mainLoop() {
@@ -39,9 +40,34 @@ private:
         }
     }
 
-    void cleanup() {
-        glfwDestroyWindow(_window);
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Basic Application";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
 
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        if (vkCreateInstance(&createInfo, nullptr, &_instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create an instance!");
+        }
+    }
+
+    void cleanup() {
+        vkDestroyInstance(_instance, nullptr);
+
+        glfwDestroyWindow(_window);
         glfwTerminate();
     }
 };

69e63a27ecb31199de72db505cde620f00c48be6
kturaevv <kturaevv@gmail.com>
2024-06-20 11:08:31 +0500

    add: validation layers and logging primitives

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,46 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <cstring>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+const std::vector<const char*> VALIDATION_LAYERS = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+// Vulkan debug primitives
+#ifdef NDEBUG
+    const bool enableValidationLayers = false;
+#else
+    const bool enableValidationLayers = true;
+#endif
+
+// Logging debug primitives
+#ifdef NDEBUG
+    #define LOG_INFO(...) 
+#else
+    #include <fmt/core.h>
+    #include <fmt/chrono.h>
+    #include <iostream>
+
+    #define LOG_INFO(...) fmt::print("[INFO] [{}] [{}:{}] {}\n", getCurrentTime(), __FILE__, __LINE__, fmt::format(__VA_ARGS__))
+
+    std::string getCurrentTime() {
+        auto now = std::chrono::system_clock::now();
+        return fmt::format("{:%Y-%m-%d %H:%M:%S}", fmt::localtime(std::chrono::system_clock::to_time_t(now)));
+    }
+#endif
+
 
 class BasicApplication {
 public:
 void run() {
     initWindow();
+    LOG_INFO("Window initialized");
     initVulkan();
+    LOG_INFO("Vulkan initialized");
     mainLoop();
     cleanup();
 }
@@ -41,6 +72,11 @@ private:
     }
 
     void createInstance() {
+        LOG_INFO("Validation layers check");
+        if (enableValidationLayers && !checkValidationLayerSupport()) {
+            throw std::runtime_error("validation layers requested, but not available!");
+        }
+        
         VkApplicationInfo appInfo{};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
         appInfo.pApplicationName = "Basic Application";
@@ -48,6 +84,7 @@ private:
         appInfo.pEngineName = "No Engine";
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.apiVersion = VK_API_VERSION_1_0;
+        LOG_INFO("VkApplicationInfo initialized");
 
         uint32_t glfwExtensionCount = 0;
         const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
@@ -59,11 +96,43 @@ private:
         createInfo.ppEnabledExtensionNames = glfwExtensions;
         createInfo.enabledLayerCount = 0;
 
+        if (enableValidationLayers) {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(VALIDATION_LAYERS.size());
+            createInfo.ppEnabledLayerNames = VALIDATION_LAYERS.data();
+        } 
+        LOG_INFO("VkInstanceCreateInfo initialized");
+
         if (vkCreateInstance(&createInfo, nullptr, &_instance) != VK_SUCCESS) {
             throw std::runtime_error("failed to create an instance!");
         }
+        LOG_INFO("Vulkan instance created");
     }
 
+    bool checkValidationLayerSupport() {
+        uint32_t layerCount;
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+        std::vector<VkLayerProperties> availableLayers(layerCount);
+        vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+        for (const char* layerName : VALIDATION_LAYERS) {
+            bool layerFound = false;
+
+            for (const auto& layerProperties : availableLayers) {
+                if (strcmp(layerName, layerProperties.layerName) == 0) {
+                    layerFound = true;
+                    break;
+                }
+            }
+
+            if (!layerFound) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
     void cleanup() {
         vkDestroyInstance(_instance, nullptr);
 

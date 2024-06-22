f9a938af452afbdbdafc38042d066b972d43daa4
kturaevv <kturaevv@gmail.com>
2024-06-22 09:26:26 +0500

    add: image views

--- src/main.cpp
+++ src/main.cpp
@@ -81,6 +81,8 @@ private:
     VkExtent2D _swapChainExtent;
     std::vector<VkImage> _swapChainImages;
 
+    std::vector<VkImageView> _swapChainImageViews;
+
 private:
     void initWindow() {
         glfwInit();
@@ -97,6 +99,8 @@ private:
         this->pickPhysicalDevice();
         this->createLogicalDevice();
         this->createSwapChain();
+        this->createImageViews();
+
         LOG("Vulkan initialized");
     }
 
@@ -425,7 +429,37 @@ private:
         }
     }
 
+    void createImageViews() {
+        this->_swapChainImageViews.resize(this->_swapChainImages.size());
+        
+        for (size_t i = 0; i < this->_swapChainImages.size(); i++) {
+            VkImageViewCreateInfo createInfo{};
+            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            createInfo.image = this->_swapChainImages[i];
+            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            createInfo.format = this->_swapChainImageFormat;
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            createInfo.subresourceRange.baseMipLevel = 0;
+            createInfo.subresourceRange.levelCount = 1;
+            createInfo.subresourceRange.baseArrayLayer = 0;
+            createInfo.subresourceRange.layerCount = 1;
+            if (vkCreateImageView(this->_device, &createInfo, nullptr, &this->_swapChainImageViews[i]) != VK_SUCCESS) {
+                throw std::runtime_error("failed to create image views!");
+            }
+        }    
+
+        LOG("Image views have been created")
+    }
+
     void cleanup() {
+        for (auto imageView : this->_swapChainImageViews) {
+            vkDestroyImageView(this->_device, imageView, nullptr);
+        }
+
         vkDestroySwapchainKHR(this->_device, this->_swapChain, nullptr);
         vkDestroyDevice(this->_device, nullptr);
         vkDestroySurfaceKHR(this->_instance, this->_surface, nullptr);
